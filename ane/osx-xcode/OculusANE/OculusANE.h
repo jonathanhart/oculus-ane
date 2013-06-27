41262e21d59d29657263b59fca05d2b108234847
Tom Goethals <fragilem17@gmail.com>
2013-06-27 08:05:07 -0700

    moved from mac to osx-xcode

---
+++ ane/osx-xcode/OculusANE/OculusANE.h
@@ -0,0 +1,10 @@
+//
+//  OculusANE.h
+//  OculusANE
+//
+//  Created by Jonathan on 5/26/13.
+//  Copyright (c) 2013 Numeda. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
---
+++ ane/osx-xcode/OculusANETests/OculusANETests.h
@@ -0,0 +1,13 @@
+//
+//  OculusANETests.h
+//  OculusANETests
+//
+//  Created by Jonathan on 5/26/13.
+//  Copyright (c) 2013 Numeda. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface OculusANETests : SenTestCase
+
+@end
