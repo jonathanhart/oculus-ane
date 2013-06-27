1b6b152bd40c84ca5a86dd600729e4fd7a9537b5
Tom Goethals <fragilem17@gmail.com>
2013-06-27 09:03:01 -0700

    modified the build.bat in orde to be able to compile the ANE again. same thing still has to be done to the build.sh

--- ane/build/build.bat
+++ ane/build/build.bat
@@ -1,17 +1,32 @@
-del OculusANE.ane
-del OculusANE.swc
-del mac /Q
-del win /Q
+del ../bin
 
+REM // delete the previous build stuff
+md ..\bin
 md mac
 md win
 
-7z e ../ane/bin/OculusANE.swc -y
+REM // unzip the swc library file
+7z e ../as3/bin/OculusANE.swc -y
+
+REM // copy the library into the specific implementation folder (mac, win)
 copy library.swf mac
 copy library.swf win
-xcopy "..\ane\bin\OculusANE.swc" ".\OculusANE.swc"
 
-xcopy "../mac/OculusANE/build/Release" "mac\" /S
-xcopy "../windows/OculusANE/Release" "win\"
-call "C:/Program Files (x86)/FlashDevelop/Tools/flexsdk/bin/adt" -package -target ane OculusANE.ane extension.xml -swc OculusANE.swc -platform MacOS-x86 -C mac . -platform Windows-x86 -C win . 
+REM // copy the swc here
+xcopy "..\as3\bin\OculusANE.swc" "." /Y
+
+REM // copy the mac build files here
+xcopy "..\osx-xcode\build\Release" "mac\" /S /Y
+
+REM // copy the windows build files here
+xcopy "..\win-visualstudio\Release" "win\" /S /Y
+
+REM // call the build script on adt
+call "C:/Program Files (x86)/FlashDevelop/Tools/flexsdk/bin/adt" -package -target ane ../bin/OculusANE.ane extension.xml -swc OculusANE.swc -platform MacOS-x86 -C mac . -platform Windows-x86 -C win . 
+
+del OculusANE.swc
+del catalog.xml
+rd mac /S /Q
+rd win /S /Q
+
 PAUSE
