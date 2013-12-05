5513fcefdba05efe34f1aa9b88eec521649872a9
Jonathan <jonathan@numeda.com>
2013-12-04 16:39:49 -0800

    upgraded to oculus sdk 0.2.5c and fixed build script

--- ane/build/build.sh
+++ ane/build/build.sh
@@ -1,9 +1,12 @@
 rm OculusANE.ane
 rm OculusANE.swc
-cp ../ane/bin/OculusANE.swc .
+cp ../OculusANE.swc .
 unzip -o OculusANE.swc
 rm -r -f mac
 mkdir mac
-cp -L -R "../mac/OculusANE/build/Release/" mac
+cp -L -R "../osx-xcode/build/Release/" mac
 cp library.swf mac
-"/Applications/Adobe Flash Builder 4.7/eclipse/plugins/com.adobe.flash.compiler_4.7.0.349722/AIRSDK/bin/adt" -package -target ane OculusANE.ane extension.xml -swc OculusANE.swc -platform MacOS-x86 -C mac . 
+mkdir windows
+cp library.swf windows
+cp ../win-visualstudio/Release/* windows
+"/Applications/Adobe Flash Builder 4.7/eclipse/plugins/com.adobe.flash.compiler_4.7.0.349722/AIRSDK/bin/adt" -package -target ane OculusANE.ane extension.xml -swc OculusANE.swc -platform MacOS-x86 -C mac . -platform Windows-x86 -C windows .
--- ane/osx-xcode/build/OculusANE.build/Release/OculusANE.build/Objects-normal/i386/OculusANE.d
+++ ane/osx-xcode/build/OculusANE.build/Release/OculusANE.build/Objects-normal/i386/OculusANE.d
@@ -1,6 +1,6 @@
 dependencies: \
-  /Users/jonathan/Documents/git/oculus-ane/mac/OculusANE/OculusANE/OculusANE.mm \
-  /Users/jonathan/Documents/git/oculus-ane/mac/OculusANE/OculusANE/OculusANE.h \
+  /Users/jonathan/Documents/git/oculus-ane/ane/osx-xcode/OculusANE/OculusANE.mm \
+  /Users/jonathan/Documents/git/oculus-ane/ane/osx-xcode/OculusANE/OculusANE.h \
   /Applications/Adobe\ Flash\ Builder\ 4.7/eclipse/plugins/com.adobe.flash.compiler_4.7.0.349722/AIRSDK/runtimes/air/mac/Adobe\ AIR.framework/Headers/Adobe\ Air.h \
   /Applications/Adobe\ Flash\ Builder\ 4.7/eclipse/plugins/com.adobe.flash.compiler_4.7.0.349722/AIRSDK/runtimes/air/mac/Adobe\ AIR.framework/Headers/FlashRuntimeExtensions.h \
   ../../oculus_sdk/OculusSDK/LibOVR/include/OVR.h \
@@ -9,6 +9,7 @@ dependencies: \
   ../../oculus_sdk/OculusSDK/LibOVR/include/../Src/Kernel/OVR_Log.h \
   ../../oculus_sdk/OculusSDK/LibOVR/include/../Src/Kernel/OVR_Math.h \
   ../../oculus_sdk/OculusSDK/LibOVR/include/../Src/Kernel/OVR_RefCount.h \
+  ../../oculus_sdk/OculusSDK/LibOVR/include/../Src/Kernel/OVR_Std.h \
   ../../oculus_sdk/OculusSDK/LibOVR/include/../Src/Kernel/OVR_System.h \
   ../../oculus_sdk/OculusSDK/LibOVR/include/../Src/OVR_Device.h \
   ../../oculus_sdk/OculusSDK/LibOVR/Src/OVR_DeviceConstants.h \
@@ -21,11 +22,10 @@ dependencies: \
   ../../oculus_sdk/OculusSDK/LibOVR/Src/Kernel/OVR_Atomic.h \
   ../../oculus_sdk/OculusSDK/LibOVR/Src/Kernel/OVR_String.h \
   ../../oculus_sdk/OculusSDK/LibOVR/include/../Src/Kernel/OVR_UTF8Util.h \
-  ../../oculus_sdk/OculusSDK/LibOVR/include/../Src/Kernel/OVR_Std.h \
   ../../oculus_sdk/OculusSDK/LibOVR/include/../Src/Kernel/OVR_Alg.h \
   ../../oculus_sdk/OculusSDK/LibOVR/include/../Src/OVR_SensorFusion.h \
   ../../oculus_sdk/OculusSDK/LibOVR/Src/OVR_SensorFilter.h \
+  ../../oculus_sdk/OculusSDK/LibOVR/include/../Src/OVR_Profile.h \
   ../../oculus_sdk/OculusSDK/LibOVR/include/../Src/Util/Util_LatencyTest.h \
   ../../oculus_sdk/OculusSDK/LibOVR/include/../Src/Util/../Kernel/OVR_List.h \
-  ../../oculus_sdk/OculusSDK/LibOVR/include/../Src/Util/Util_Render_Stereo.h \
-  ../../oculus_sdk/OculusSDK/LibOVR/include/../Src/Util/Util_MagCalibration.h
+  ../../oculus_sdk/OculusSDK/LibOVR/include/../Src/Util/Util_Render_Stereo.h
