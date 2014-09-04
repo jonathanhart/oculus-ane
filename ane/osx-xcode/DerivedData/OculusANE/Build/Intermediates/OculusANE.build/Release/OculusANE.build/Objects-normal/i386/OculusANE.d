7b7b3a91cd8c3189bbbc60349d90f898f2668e97
Jonathan Hart <jonathan.hart@gmail.com>
2014-09-04 11:48:12 -0700

    Added release version of the Mac ANE library

---
+++ ane/osx-xcode/DerivedData/OculusANE/Build/Intermediates/OculusANE.build/Release/OculusANE.build/Objects-normal/i386/OculusANE.d
@@ -0,0 +1,6 @@
+dependencies: \
+  /Users/jonathanhart/Desktop/numeda_projects/ext/oculus-ane/ane/osx-xcode/OculusANE/OculusANE.mm \
+  /Users/jonathanhart/Desktop/numeda_projects/ext/oculus-ane/ane/osx-xcode/OculusANE/OculusANE.h \
+  /Applications/Adobe\ Flash\ Builder\ 4.7/eclipse/plugins/com.adobe.flash.compiler_4.7.0.349722/AIRSDK/runtimes/air/mac/Adobe\ AIR.framework/Headers/Adobe\ Air.h \
+  /Applications/Adobe\ Flash\ Builder\ 4.7/eclipse/plugins/com.adobe.flash.compiler_4.7.0.349722/AIRSDK/runtimes/air/mac/Adobe\ AIR.framework/Headers/FlashRuntimeExtensions.h \
+  ../../oculus_sdk_041/OculusSDK/LibOVR/Src/OVR_CAPI.h
