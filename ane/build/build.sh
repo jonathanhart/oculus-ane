27f20af3c04b808202fb58d2a4b08a9ed92cce6f
Jonathan Hart <jonathan.hart@gmail.com>
2014-09-04 10:25:38 -0700

    Updated build script to package Win side of ANE via Mac

--- ane/build/build.sh
+++ ane/build/build.sh
@@ -8,5 +8,5 @@ cp -L -R "../osx-xcode/DerivedData/OculusANE/Build/Products/Debug/" mac
 cp library.swf mac
 mkdir windows
 cp library.swf windows
-cp ../win-visualstudio/Release/* windows
+cp ../win-visualstudio/OculusANE_041/Release/OculusANE.dll windows
 "/Applications/Adobe Flash Builder 4.7/eclipse/plugins/com.adobe.flash.compiler_4.7.0.349722/AIRSDK/bin/adt" -package -target ane OculusANE.ane extension.xml -swc OculusANE.swc -platform MacOS-x86 -C mac . -platform Windows-x86 -C windows .
