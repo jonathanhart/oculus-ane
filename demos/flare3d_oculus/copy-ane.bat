df927c3c2376684c7eaee01a45ec4946bb7f456f
Tom Goethals <fragilem17@gmail.com>
2014-09-22 09:35:51 +0200

    building scripts, update to Air SDK 15

--- demos/flare3d_oculus/copy-ane.bat
+++ demos/flare3d_oculus/copy-ane.bat
@@ -1,4 +1,4 @@
 rmdir /s /q ext\debug\OculusANE.ane
-xcopy "C:\git\oculus-ane\ane\bin\OculusANE.ane" "ext\release" /S /Y
+xcopy "C:\projects\oculus-ane\oculus-ane\demos\flare3d_oculus\ane\bin\OculusANE.ane" "ext\release" /S /Y
 7z x ext/release/OculusANE.ane -y -oext/debug/OculusANE.ane
 ::pause
--- demos/flare3d_oculus/run.bat
+++ demos/flare3d_oculus/run.bat
@@ -1,3 +1,3 @@
-"C:\Users\Tom\AppData\Local\FlashDevelop\Apps\ascsdk\14.0.0\bin\adl.exe" oculus.xml bin -extdir ext\debug
+"C:\Users\tomg.BUTBEL\AppData\Local\FlashDevelop\Apps\ascsdk\15.0.0\bin\adl.exe" oculus.xml bin -extdir ext\debug
 pause
 ::"C:\Program Files (x86)\FlashDevelop\Tools\ascsdk\bin\adt.exe" -package -tsa none %SIGNING_OPTIONS% %OUTPUT% %APP_XML% %FILE_OR_DIR%
